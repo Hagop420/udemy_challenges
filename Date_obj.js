df211eb8c76e782754ee71ebd629f32cc1a2ab55
Hagop420 <jacka161swe@gmail.com>
2023-06-14 15:28:29 -0700

    Date object closer look

---
+++ Date_obj.js
@@ -0,0 +1,39 @@
+// let d
+// // Date object
+// d=new Date()
+
+
+// // mutations
+// // issue because of the time zones
+// d = new Date('2022-07-10')
+// // issue ending
+
+
+// // d = new Date('02-10-2039')
+// // current time stamp
+// d = Date.now()
+
+
+// // d = new Date('03-49-3021')
+
+
+// d = Date().valueOf(1686780290756)
+// d = new Date(1686780290756)
+// d.setMonth('02')
+// d.setFullYear('1003')
+// d.setMinutes('22')
+// console.log(d);
+
+
+let t=new Date()
+
+// t = t.valueOf()
+
+// // timestap in seconds
+
+// t=Math.floor(new Date().valueOf()/100)
+
+t.setMilliseconds('13')
+t.setHours('10')
+t=t.getHours()
+console.log(t);
