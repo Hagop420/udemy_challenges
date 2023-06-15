52308d198776cee1135dfc9727b1ec16e2d38f5c
Hagop420 <jacka161swe@gmail.com>
2023-06-15 11:58:31 -0700

    Arrays and Objects closer look

---
+++ Arrays_Objects.js
@@ -0,0 +1,47 @@
+// Creating an array
+
+let x;
+
+const nums = [1, 2, 3, 4, 5]
+
+
+
+
+// methods to access specific nums or indecies in array
+
+x=nums[0]
+x=nums[1] + nums[2]
+x = `Hi, im adding nums[1] nums[2], so in this case its 2+3 which equals to, ${nums[1] + nums[2]}`
+
+
+// length property on array
+
+
+x = nums.length
+
+
+
+// changing a number in the array
+
+
+nums[2] = 2
+
+x=nums
+
+// old way to make arrays
+
+const str1=new Array('Apple' , 'green' , 'orange' , 'taco')
+
+
+
+
+// old array logging
+
+// console.log(str1);
+
+
+
+
+console.log(x);
+
+// Creating an object
